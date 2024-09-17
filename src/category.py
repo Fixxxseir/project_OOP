b8de4f06a2fd29db07779bcf0e9102f5ef220b8e
mexic <mexic2284@mail.ru>
2024-09-17 19:39:48 +0300

    make function products, products_list, magic __str__, __repr__

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Класс представления категории продуктов."""
 
@@ -10,12 +13,40 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
+
+    def add_product(self, product):
+        if isinstance(product, Product):
+            for product in self.__products:
+                if product.name == product.name:
+                    product.quantity += product.quantity
+                    if product.price < product.price:
+                        product.price = product.price
+                    return
+        else:
+            raise TypeError("Продукт должен быть экземпляром класса Product")
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """Список товаров в виде строк в формате"""
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт. "
+        return product_str
+
+    @property
+    def products_list(self):
+        return self.__products
 
     def __repr__(self):
-        return f"Category: {self.name}, products: {self.products}"
+        return f"Category: {self.name}, products: {self.__products}"
 
     def __str__(self):
-        return f"Category: {self.name}, Products: {', '.join(str(p) for p in self.products)}"
+        total_quantity = 0
+        for item in self.__products:
+            total_quantity += item.quantity
+        return f"{self.name}, количество продуктов: {total_quantity} шт."
