32ce9f62c1c97b20e30bdd56f30c242b0c334eb6
mexic <mexic2284@mail.ru>
2024-09-21 16:59:53 +0300

    make new module category_iterator.py

---
+++ src/category_iterator.py
@@ -0,0 +1,35 @@
+from src.category import Category
+from src.product import Product
+
+
+class IterCategory:
+    def __init__(self, category):
+        self.category = category
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.category.products_list):
+            obj = self.category.products_list[self.index]
+            self.index += 1
+            return obj
+        else:
+            raise StopIteration
+
+
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    iterator = IterCategory(category1)
+    for x in iterator:
+        print(x)
