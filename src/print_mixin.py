c894a8b10ac57e7de5e8b637cea76f3b2540e548
mexic <mexic2284@mail.ru>
2024-10-03 22:14:55 +0300

    make additional class for info magic method

---
+++ src/print_mixin.py
@@ -0,0 +1,7 @@
+class PrintMixin:
+	def __init__(self):
+		""" Класс миксин, при создании объекта выводит в консоль информацию об объекте"""
+		print(repr(self))
+
+	def __repr__(self):
+		return f'{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})'
